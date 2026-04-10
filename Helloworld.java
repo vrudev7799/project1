5da582e9d3c10c2e2462506da6a78203b489e38e
root <root@ip-172-31-64-228.ec2.internal>
2026-04-10 08:00:34 +0000

    Helloworld java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 // Save this file as HelloWorld.java
-public class HelloWorld {
+public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, world!");
     }
 }
 
